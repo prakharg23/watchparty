7f3171a36dfa8e69311a37b9faae00c22833f29c
Prakhar Gautam <42948914+prakharg23@users.noreply.github.com>
2026-09-08 19:59:52 -0400

    Default the extension to the deployed relay

--- extension/config.js
+++ extension/config.js
@@ -2,6 +2,6 @@
 // so friends never need to touch settings. If you want your own build to point
 // at your relay out of the box, set DEFAULT_SERVER_URL to your wss:// address.
 const WP_CONFIG = {
-  DEFAULT_SERVER_URL: "ws://localhost:8080",
+  DEFAULT_SERVER_URL: "wss://watchparty-relay-9om9.onrender.com",
   DEFAULT_SECRET: "",
 };
